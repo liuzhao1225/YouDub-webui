a2f8813763b17fe50db5064ac6de752b36da603f
41525 <415256473@qq.com>
2024-10-31 14:10:21 +0800

    增加多用户

---
+++ youdub/test.py
@@ -0,0 +1,13 @@
+import os
+
+def print_directory_tree(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * level
+        print(f"{indent}{os.path.basename(root)}/")
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print(f"{subindent}{f}")
+
+# 使用当前目录
+print_directory_tree('..')
